092106878be280e3ba0edcb3fc9cc8d4b2146a0e
Pierre-Yves Chibon <pingou@pingoured.fr>
2014-04-19 10:11:13 +0200

    Add the setup.py script

---
+++ setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+"""
+Setup script
+"""
+
+from gitsync import __version__
+from setuptools import setup
+
+
+def get_requires():
+    ''' Reads the requirements.txt and return its content in a list. '''
+    stream = open('requirements.txt')
+    content = stream.readlines()
+    stream.close()
+
+    deps = []
+    for row in content:
+        if row.startswith('#'):
+            continue
+        else:
+            deps.append(row.strip())
+
+    return deps
+
+requires = get_requires()
+
+setup(
+    name='gitsync',
+    version=__version__,
+    author='Pierre-Yves Chibon',
+    author_email='pingou@pingoured.fr',
+    url='https://github.com/pypingou/gitsync',
+    py_modules=['gitsync'],
+    install_requires=requires,
+    entry_points={
+        'console_scripts': [
+            "gitsync-cli=gitsync:main",
+        ]
+    },
+)
